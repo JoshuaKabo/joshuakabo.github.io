fb86ea249585f6958be72aa7f9b96ad3fab8148d
Josh Kabo <joshkong1@gmail.com>
2023-04-19 12:26:10 -0700

    siteurl in config fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `Joshua Kabo`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-sass"]
+	siteMetadata: {
+		title: `Joshua Kabo`,
+		siteUrl: `https://joshuakabo.github.io`,
+	},
+	plugins: ["gatsby-plugin-sass"],
 };
