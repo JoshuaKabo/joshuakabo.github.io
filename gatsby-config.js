dd6fd4964bfb31347c04112ac209f92c2e9bbab8
Josh Kabo <joshkong1@gmail.com>
2023-04-19 12:13:02 -0700

    Gatsby is setup

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Joshua Kabo`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
