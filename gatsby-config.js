1cc9edef6e9a55d5b48d22f99f5061b532fc51ee
Josh Kabo <joshkong1@gmail.com>
2023-07-23 16:16:42 -0700

    fix config deletion

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,9 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  plugins: [],
-}
+  siteMetadata: {
+    title: `Joshua Kabo`,
+    siteUrl: `https://joshuakabo.github.io`,
+  },
+  plugins: ["gatsby-plugin-sass"],
+};
