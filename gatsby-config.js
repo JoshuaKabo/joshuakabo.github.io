102497f4165342a0cdd590717216a02e070a447c
Josh Kabo <joshkong1@gmail.com>
2023-07-23 15:48:24 -0700

    getting started, assets, index, favicon, css
    index is contact page, start on colors and use of favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,12 @@
 /**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-	siteMetadata: {
-		title: `Joshua Kabo`,
-		siteUrl: `https://joshuakabo.github.io`,
-	},
-	plugins: ["gatsby-plugin-sass"],
-};
+  plugins: [],
+}
